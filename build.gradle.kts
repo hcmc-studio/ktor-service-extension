c74bdcfd0246a0d39bc4a4b6b78cebc1cc5eb13d
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-15 17:59:25 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,12 @@ publishing {
             version = project_version
             from(components["java"])
         }
+        create<MavenPublication>("jitpack") {
+            groupId = "com.github.hcmc-studio"
+            artifactId = project.name
+            version = project_version
+            from(components["java"])
+        }
     }
 }
 
